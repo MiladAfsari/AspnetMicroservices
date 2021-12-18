10028e319410bc18d4064de23d403124e8d626ab
Mili <Miliajax69@gmail.com>
2021-12-18 08:01:38 +0330

    Implement CQRS Design Pattern

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} Is Required")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} Must Not Exceed 50 Characters");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} Is Required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} Is Required")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero");
+
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator :AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+              .NotEmpty().WithMessage("{UserName} Is Required")
+              .NotNull()
+              .MaximumLength(50).WithMessage("{UserName} Must Not Exceed 50 Characters");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} Is Required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} Is Required")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero");
+        }
+    }
+}
